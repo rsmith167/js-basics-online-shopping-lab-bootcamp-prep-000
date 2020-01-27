b23724b877e542f693e4bd46a8a724f8482219d4
Rwanda S <rwanda.ace@gmail.com>
2020-01-27 17:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
   for(let i = 0; i < cart.length -1; i++){
     itemAndPrice.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
   }
-  itemAndPrice.push(`and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}.`)
+  itemAndPrice.push(` and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}.`)
   totalMessage.push(itemAndPrice)
   return `${beginningMessage} ${itemAndPrice}`
   
