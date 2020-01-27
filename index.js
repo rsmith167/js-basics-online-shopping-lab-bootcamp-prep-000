561bad17119cd04951af5124fd411f7336bb3d97
Rwanda S <rwanda.ace@gmail.com>
2020-01-27 18:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if(cart[i].itemName === item){
       indexCheck = i
-      cart.splice( i + 1, 1)
+      cart.splice( i, 1)
+      reutnr cart
     }
     
   }
