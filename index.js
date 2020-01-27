f26b7ac108b78b051cf27ca65001b05e1b61cef9
Rwanda S <rwanda.ace@gmail.com>
2020-01-27 16:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ function viewCart() {
   if(cart.length < 1){
     return "Your shopping cart is empty."
   }else if(cart.length === 1){
-   return `${beginningMessage} ${cart[0].itemName} at ${cart[0].itemPrice}.`
+   return `${beginningMessage} ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }else{
   
   for( let i = 0; i < items.length -1; i++){
-    itemAndPrice.push(` ${cart[i].itemName} at ${cart[i].itemPrice},`)
+    itemAndPrice.push(` ${cart[i].itemName} at $${cart[i].itemPrice},`)
   }
   totalMessage.push(beginningMessage)
   totalMessage.push(itemAndPrice)
-  totalMessage.push(`and ${cart[cart.length -1].itemName} at ${cart[cart.length -1].itemPrice}.`)
+  totalMessage.push(`and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}.`)
   return totalMessage
   }
 }
