df178909ff0eb9de49af3ec6e8477fa6c4b73a5e
Rwanda S <rwanda.ace@gmail.com>
2020-01-27 17:50:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,10 +64,11 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   
+  total()
+  
   if(cardNumber === null){
     console.log("Sorry, we don't have a credit card on file for you.")
   }else{
-    total()
     cart = new Array()
     return `Your total cost is ${totalPrice}, which will be charged to the card ${cardNumber}.`
   }
