154ce3de8a36f65a6eaf6a8517f17f8a39a33195
Rwanda S <rwanda.ace@gmail.com>
2020-01-27 17:52:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function placeOrder(cardNumber) {
   
   var totalPrice = total()
   
-  if(cardNumber < 1){
+  if(!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
   }else{
     cart = new Array()
